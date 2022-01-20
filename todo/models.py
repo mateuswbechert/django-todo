be1d69289107622d67990fceab3629994ecc0f34
Mateus WB <mateuswbechert@gmail.com>
2022-01-20 00:31:01 -0300

    Added status model and default values

--- todo/models.py
+++ todo/models.py
@@ -5,6 +5,17 @@ class Todo(models.Model):
     title=models.CharField(max_length=100)
     details=models.TextField()
     date=models.DateTimeField(default=timezone.now)
+    status=models.ForeignKey('Status', on_delete=models.PROTECT, blank=True, default='2')
 
     def __str__(self):
         return self.title
+
+
+class Status(models.Model):
+    title=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+
