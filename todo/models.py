7afa23e715d521a210ced092a868e4415f91603a
Mateus WB <mateuswbechert@gmail.com>
2022-02-01 13:02:18 -0300

    Created models for data relation: Board, BoardUser, CardUser

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.db import models
+from django.conf import settings
 
 class Todo(models.Model):
     title=models.CharField(max_length=100)
@@ -19,4 +20,20 @@ class Status(models.Model):
         return self.title
 
 
+class Board(models.Model):
+    title=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
 
+
+class BoardUser(models.Model):
+    board_id=models.ForeignKey('Board', on_delete=models.PROTECT)
+    user_id=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_board_id=models.IntegerField(default=0)
+
+
+class CardUser(models.Model):
+    card_id=models.ForeignKey('Todo', on_delete=models.PROTECT)
+    user_id=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_card_id=models.IntegerField(default=0)
